ad292b495dbec51ee928c1cf6ff63a2d00964937
Aman Kumar <96618245+Aman123x@users.noreply.github.com>
2023-10-25 03:34:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+ // Function to be executed when the DOM is loaded
+      function onDOMLoad() {
+        // Get the body element
+        var body = document.body;
+
+        // Clear the body's inner HTML to make it empty
+        body.innerHTML = '';
+
+        // Create a new text node with the content "DOM load success"
+        var textNode = document.createTextNode("DOM load success");
+
+        // Append the text node to the body
+        body.appendChild(textNode);
+      }
+
+      // Add an event listener for the DOMContentLoaded event
+      document.addEventListener("DOMContentLoaded", onDOMLoad);
